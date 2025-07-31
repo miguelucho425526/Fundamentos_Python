14da6e3f2c9641624fda423aa88d7d114631611b
Miguel <aguirremorenomiguelangel2008@gmail.com>
2025-07-31 15:16:00 -0500

    Tercer laboratorio de la seccion 1 realizado

---
+++ src/Seccion 1/LAB la funcion print() y sus argumentos.py
@@ -0,0 +1,4 @@
+print("Programming", "Essentials", "in...", sep="***", end="")
+print("Python")
+
+#La función print() muestra varios textos en pantalla, separándolos con "***" en vez del espacio habitual, y termina la línea sin salto, permitiendo que el siguiente print() continúe en la misma línea.
