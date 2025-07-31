c39e5687633cfbc4a37aceff0fb1756dd33381f3
Miguel <aguirremorenomiguelangel2008@gmail.com>
2025-07-31 15:14:24 -0500

    Segundo laboratorio de la seccion 1 realizado

---
+++ src/Seccion 1/LAB Dando Formato a la salida.py
@@ -0,0 +1,13 @@
+# Flecha doble de tamaño y duplicada horizontalmente, usando menos print()
+arrow = (
+    "        **        **         ** \n"
+    "       *  *      *  *       *   * \n"
+    "      *    *    *    *     *     * \n"
+    "     *      *  *      *   *       * \n"
+    "****        ****        ****    ****\n"
+    "      *    *    *    *     *     * \n"
+    "      *    *    *    *     *     * \n"
+    "      ******    ******     *******\n"
+)
+
+#Se está creando una variable llamada arrow que contiene un dibujo de una flecha doble, grande y duplicada horizontalmente, usando una sola cadena de texto multilínea. Así se evita usar múltiples instrucciones print().
